eb94b2ec418b4dc26cee1b071c4e39e0faf78b35
thomas <thomasbrushel321@gmail.com>
2018-08-21 07:30:28 -0500

    added in some options to change the font-size and color.
    In addition, I also added in some defaultProps so if you don't add anything it doesn't show up as undefined in the markup

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -5,24 +5,33 @@ const Hero = (props) => {
   const { 
     src, 
     altText, 
-    heroTitle, 
+    heroTitle,
+    textHeadingColor,
+    textLeadColor,
+    textLeadSize,
+    textHeadingSize,
     heroIntro,
     heroTextPosition 
   } = props;
-
+  
   return (
     <div>
       <div className="hero">
         <img src={ src } alt={ altText }/>
         <div className="container">
           <div className={`hero-text ${heroTextPosition}`}>
-            <h1>{ heroTitle }</h1>
-            <p>{ heroIntro }</p>
+            <h1 className={`mo-text--${textHeadingSize}`} style={{color: `${textHeadingColor}`}}>{ heroTitle }</h1>
+            <p className={`mo-text--${textLeadSize}`} style={{color: `${textLeadColor}`}}>{ heroIntro }</p>
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+Hero.defaultProps = {
+  textHeadingSize: "md",
+  textLeadSize: "sm"
 };
 
 export default Hero;
