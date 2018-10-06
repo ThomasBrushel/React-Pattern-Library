2d3e97e234dcea2d6214fe8341d40ba66eb00593
thomas <thomasbrushel321@gmail.com>
2018-10-06 08:39:04 -0500

    added new class based component

---
+++ src/components/layout/HeroParallax.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HeroParallax extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default HeroParallax;
